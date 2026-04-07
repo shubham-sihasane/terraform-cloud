d8e4310cab904ee717aae6007e68c706afa2fe24
Shubham Sihasane <78297361+shubham-sihasane@users.noreply.github.com>
2026-04-08 02:25:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "instance_ami" {
   type = string
   description = "The default EC2 AMI"
-  default = "ami-05d2d839d4f73aafb"
+  # default = "ami-05d2d839d4f73aafb"
 }
 
 variable "instance_type" {
   type = string
   description = "The EC2 instance type"
-  default = "t2.nano"
+  # default = "t2.nano"
 }
